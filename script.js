5218427af3cbe219c471f18b9171f35e38f548d8
intizam11 <weirdfunny.official@gmail.com>
2023-02-09 15:42:37 +0700

    none

---
+++ script.js
@@ -0,0 +1,15 @@
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const phoneNumb = document.getElementById('phone').value;
+const select = document.getElementById('select').value;
+const message = document.getElementById('message').value;
+
+
+
+
+
+// console.log(name)
+// console.log(email)
+// console.log(phoneNumb)
+// console.log(select)
+// console.log(message)
